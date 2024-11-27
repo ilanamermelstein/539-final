bda65cbd45d1d4faecdfb4c378b5d3729fe0c073
Ilana Mermelstein <imerm@umich.edu>
2024-11-27 14:11:23 -0500

    Add files via upload

--- js/finalproj.js
+++ js/finalproj.js
@@ -1,3 +1,6 @@
+import musicals from '/musicals.json';
+console.log("Musicals loaded");
+
 function hamburgerMenu() {
     const x = document.querySelector("#navLinks");
     if (x.style.display === "none") {
@@ -5,5 +8,23 @@ function hamburgerMenu() {
     } else {
       x.style.display = "none";
     }
-    console.log("Hamburger Menu Clicked")
-  }
+    console.log("Hamburger Menu Clicked");
+  };
+
+document.getElementById("playHome").addEventListener("click", roundOne);
+document.getElementById("playAgain").addEventListener("click", roundOne);
+document.getElementById("playInstr").addEventListener("click", roundOne);
+
+function roundOne() {
+    console.log("Round One initiated");
+    const randomIndex = Math.floor(Math.random() * musicals.length);
+    const roundOneMusical = musicals[randomIndex][1];
+    const roundOneYear = musicals[randomIndex][0];
+    const roundOneSummary = musicals[randomIndex[2]];
+    let currentScore = 0
+    document.getElementById("yearOfNom").innerHTML = roundOneYear;
+    document.getElementById("musicalSummary").innerHTML = musicalSummary;
+    document.getElementById("round").innerHTML = "1";
+    document.getElementById("currentScore").innerHTML = currentScore;
+    console.log("Round One")
+}
